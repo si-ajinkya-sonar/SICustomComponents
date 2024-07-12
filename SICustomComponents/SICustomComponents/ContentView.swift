7c5535643de49b37c1a1a1b470791d06aacddfdc
Ajinkya Sonar <ajinkya.sonar@sportzinteractive.net>
2024-07-12 12:46:56 +0530

    Initial Project Setup Added

---
+++ SICustomComponents/SICustomComponents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SICustomComponents
+//
+//  Created by Nausheen Khan on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SICustomComponents/SICustomComponents/SICustomComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SICustomComponentsApp.swift
+//  SICustomComponents
+//
+//  Created by Nausheen Khan on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SICustomComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
